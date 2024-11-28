0edcaf73a872307055d60f477f6aa87a251d46b5
rakib12212 <rakibulislamr83@gmail.com>
2024-11-29 00:25:52 +0600

    service details page created

--- src/services/getServices.js
+++ src/services/getServices.js
@@ -4,7 +4,7 @@ export const getServices = async () => {
   return services;
 };
 
-export const getServicesDetails = async () => {
+export const getServicesDetails = async (id) => {
   const res = await fetch(`http://localhost:3000/services/api/${id}`);
   const service = res.json();
   return service;
